3858618e8ae8ef5736736a8afe48fc3545accd1a
Daniel Feliciano <dani.felic7@gmail.com>
2022-08-15 17:24:20 -0300

    labs day 3

---
+++ Day 3/selecting_amino.py
@@ -0,0 +1,11 @@
+seq = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+
+seq1 = seq[0:24]
+seq2 = seq[24:54]
+seq3 = seq[54:89]
+seq4 = seq[89:110]
+
+print(seq1)
+print(seq2)
+print(seq3)
+print(seq4)
